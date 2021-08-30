23c95f2e62c25ef1dabac0324752595e811014ea
Preshh0 <uniquep201@gmail.com>
2021-08-30 03:25:19 +0100

    style files.

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index) # For our-domain.com/meetups, this should beome active
+    path('meetups/', views.index, name='all-meetups'), # For our-domain.com/meetups, this should beome active
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail')
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -4,11 +4,30 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     meetups = [
-        {'title': 'A first Meetup'},
-        {'title': 'A second Meetup'}
+        {
+            'title': 'A first Meetup', 
+            'location':'Abuja', 
+            'slug':'a-first-meetup'
+        },
+
+        {
+            'title': 'A second Meetup', 
+            'location':'Lagos', 
+            'slug':'a-second-meetup'
+        }
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups':True,
         'meetups': meetups
-    
+    })
+
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup.'
+        } 
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title':selected_meetup['title'],
+        'meetup_description':selected_meetup['description']
     })
